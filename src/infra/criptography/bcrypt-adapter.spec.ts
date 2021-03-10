f654f6d882be1e960b2fda24802f3fc587d2e432
Igor Marini Jaekel Quevedo <igor.quevedo@sensedia.com>
2021-03-10 17:27:12 -0300

    test: ensure BcryptAdapter returns a hashed value

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hashed_value'
+  }
+}))
+
 describe('BcryptAdapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const salt = 12
@@ -11,5 +17,14 @@ describe('BcryptAdapter', () => {
     await sut.encrypt('any_value')
 
     expect(spyOnBcryptHash).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hashed value on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hashed_value')
   })
 })
