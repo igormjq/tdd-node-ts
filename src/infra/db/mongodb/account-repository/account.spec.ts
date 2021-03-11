181e3eef677b892d2a9d1d92786c7d75576f4768
Igor Marini Jaekel Quevedo <igor.quevedo@sensedia.com>
2021-03-11 15:53:58 -0300

    refactor: decrease number of expects in test block

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,17 +1,15 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
-describe('Account Mongo Repository', () => {
-  beforeAll(async () => {
-    console.log('mongo?!', process.env.MONGO_URL)
+const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
 
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
 
   afterAll(async () => await MongoHelper.disconnect())
 
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
 
     const account = await sut.add({
       name: 'any_name',
@@ -22,8 +20,10 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
 
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_hashed_password')
+    expect(account).toMatchObject({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_hashed_password'
+    })
   })
 })
