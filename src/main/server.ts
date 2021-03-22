861c2917b8ba83a822978f27d7be4796ab70079d
Igor Marini Jaekel Quevedo <igor.quevedo@sensedia.com>
2021-03-22 11:15:54 -0300

    feat: connect to mongodb fefore app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
 import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(5050, () => console.log('Server up and running at http://localhost:5050'))
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    app.listen(env.port, () => console.log(`Server up and running at http://localhost:${env.port}`))
+  })
